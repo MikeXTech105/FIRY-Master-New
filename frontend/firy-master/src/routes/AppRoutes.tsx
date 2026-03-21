d36c18d4aa70fc14733fdb6156d2d640748c34c0
sahiljayani <66293567+sahiljayani@users.noreply.github.com>
2026-03-21 23:14:44 +0530

    All Protectd route changes

--- frontend/firy-master/src/routes/AppRoutes.tsx
+++ frontend/firy-master/src/routes/AppRoutes.tsx
@@ -1,24 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/auth/Login";
+import CreateUser from "../pages/auth/CreateUser";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Roles from "../pages/roles/Roles";
 import Candidate from "../pages/candidate/candidate";
 import Emails from "../pages/email/Emails";
 import EmailSettings from "../pages/emailSettings/EmailSettings";
-import CreateUser from "../pages/auth/Createuser";
+import ProtectedRoute from "./Protectedroute";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+
+
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-         <Route path="/create-user" element={<CreateUser />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/roles" element={<Roles />} />
-        <Route path="/candidate" element={<Candidate />} />
-        <Route path="/emails" element={<Emails />} />
-        <Route path="/email-settings" element={<EmailSettings />} />
+        <Route path="/create-user" element={<CreateUser />} />
+
+
+        <Route path="/dashboard" element={
+          <ProtectedRoute><Dashboard /></ProtectedRoute>
+        } />
+        <Route path="/roles" element={
+          <ProtectedRoute><Roles /></ProtectedRoute>
+        } />
+        <Route path="/candidate" element={
+          <ProtectedRoute><Candidate /></ProtectedRoute>
+        } />
+        <Route path="/emails" element={
+          <ProtectedRoute><Emails /></ProtectedRoute>
+        } />
+        <Route path="/email-settings" element={
+          <ProtectedRoute><EmailSettings /></ProtectedRoute>
+        } />
+
       </Routes>
     </BrowserRouter>
   );
